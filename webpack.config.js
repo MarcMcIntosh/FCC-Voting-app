56bbab46689ad038a0ea26e23e2a28efcda92f5e
Marc McIntosh <email@marcmcintosh.ninja>
2016-10-27 16:20:11 +0000

    Changes to be committed: 	modified:   package.json 	new file:   src/client/index.jsx 	new file:   src/server/index.js 	new file:   webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [];
