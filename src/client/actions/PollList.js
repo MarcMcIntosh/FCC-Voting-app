07256586c156fe100dad8d6317e623ec6b157c49
Marc McIntosh <email@marcmcintosh.ninja>
2016-10-31 19:34:22 +0000

    new file:   src/client/actions/PollList.js 	new file:   src/client/api/Notes.txt 	modified:   src/client/api/PollList.js

---
+++ src/client/actions/PollList.js
@@ -0,0 +1,33 @@
+import apiCall from '../api/PollList';
+
+export const RECEIVE_POLL_LIST = 'RECEIVE_POLL_LIST';
+export function receive(payload) {
+  return {
+    type: RECEIVE_POLL_LIST,
+    payload,
+  };
+}
+export const POLL_LIST_ERROR = 'POLL_LIST_ERROR';
+export function error(payload) {
+  return {
+    type: POLL_LIST_ERROR,
+    payload,
+  };
+}
+
+export const GET_POLL_LIST = 'GET_POLL_LIST';
+export function getPollList() {
+  return { type: GET_POLL_LIST };
+}
+
+export default function () {
+  return (dispatch) => {
+    dispatch(getPollList());
+    return apiCall((err, payload) => {
+      if (err) {
+        return dispatch(error(err));
+      }
+      return dispatch(receive(payload));
+    });
+  };
+}
--- src/client/api/PollList.js
+++ src/client/api/PollList.js
@@ -2,3 +2,9 @@ const DATA = [
   { id: 'aob', question: 'test' },
   { id: 'oeri', quesiton: 'test2' },
 ];
+
+const TIMEOUT = 0;
+
+export default function (callback, timeout) {
+  setTimeout(() => callback(null, DATA), timeout || TIMEOUT);
+}
